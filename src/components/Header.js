b1590ad0c306a99458294b7859ffd66d3c8af042
bruk19 <brukteshome21@gmail.com>
2022-07-05 15:26:58 +0300

    Add components

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Header = () => {
+    const headers = {
+        padding: '15px'
+    };
+    return (
+        <Header style={headers}>
+          <h1>
+            style={{
+                fontsize: '15px',
+            }}
+          </h1>
+        </Header>
+);
+};
+
+export default Header;
---
+++ src/components/TodoContainer.js
@@ -0,0 +1,71 @@
+import React from "react"
+class TodoContainer extends React.Component {
+   state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Setup development environment',
+        completed: true,
+      },
+      {
+        id: 2,
+        title: 'Develop website and add content',
+        completed: false,
+      },
+      {
+        id: 3,
+        title: 'Deploy to live server',
+        completed: false,
+      },
+    ],
+  };
+
+  delTodo = (id) => {
+    this.setState({
+      todos: [...this.state.todos.filter((todo) => todo.id !== id)],
+    });
+  };
+
+  handleChange = (id) => {
+    this.setState((prevState) => ({
+      todos: prevState.todos.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            completed: !todo.completed,
+          };
+        }
+        return todo;
+      }),
+    }));
+  };
+
+  addTodoItem = (title) => {
+    const newTodo = {
+      id: 1(),
+      title,
+      completed: false,
+    };
+    this.setState({
+      todos: [...this.state.todos, newTodo],
+    });
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <div className="inner">
+          <Header />
+          <InputTodo addTodoProps={this.addTodoItem} />
+
+          <TodosList
+            todos={this.state.todos}
+            handleChangeProps={this.handleChange}
+            deleteTodoProps={this.delTodo}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+export default TodoContainer
