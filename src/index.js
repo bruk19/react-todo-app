cc7066f0425b235e96e24b5468574c219192df73
bruk19 <brukteshome21@gmail.com>
2022-07-05 16:36:44 +0300

    Add pages for not matches

--- src/index.js
+++ src/index.js
@@ -5,9 +5,12 @@ import {
   Route,
 } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
-import TodoContainer from './functionBased/components/TodoContainer';
-import './src/App.css';
-
+import TodoContainer from '../components/TodoContainer';
+import './styles/index.css';
+import NotMatch from './pages/NotMatch';
+import About from './pages/About';
+import Navbar from './components/Navbar';
+import SinglePage from '../pages/SinglePage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
