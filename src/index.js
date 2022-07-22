551daf8c9967c3027644edb1601644be498fc73e
bruk19 <brukteshome21@gmail.com>
2022-07-22 22:29:36 +0300

    Remove unwanted files

--- src/index.js
+++ src/index.js
@@ -1,29 +1,10 @@
 import React from 'react';
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
-import TodoContainer from '../components/TodoContainer';
-import './styles/index.css';
-import NotMatch from './pages/NotMatch';
-import About from './pages/About';
-import Navbar from './components/Navbar';
-import SinglePage from '../pages/SinglePage';
+import App from './App'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<TodoContainer />} />
-        <Route path="About" element={<About />}>
-          <Route path=":abt" element={<SinglePage />} />
-        </Route>
-        <Route path="*" element={<NotMatch />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>,
 );
