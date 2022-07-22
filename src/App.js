adf07eeed3876d226379419cd0fe0998daff85eb
bruk19 <brukteshome21@gmail.com>
2022-07-23 00:05:03 +0300

    Add style to header

--- src/App.js
+++ src/App.js
@@ -1,1 +1,12 @@
+import React from 'react'
+import TodoHeader from './components/TodoHeader'
+import './App.css'
+function App() {
+  return (
+    <main>
+      <TodoHeader />
+    </main>
+  )
+}
 
+export default App
