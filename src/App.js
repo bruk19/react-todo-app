69d3c66c72942ffca6fbcf0b111cc174a4058b4c
bruk19 <brukteshome21@gmail.com>
2022-07-23 01:06:21 +0300

    Add todo input form and style it

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import TodoHeader from './components/TodoHeader'
 import './App.css'
+import TodoInput from './components/TdoInput'
 function App() {
   return (
     <main>
       <TodoHeader />
+      <TodoInput />
     </main>
   )
 }
---
+++ src/components/TdoInput.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TdoInput = () => {
+  return (
+    <form className='todo-input'>
+        <input type='text' placeholder='create new tod'/>
+    </form>
+  )
+}
+
+export default TdoInput
